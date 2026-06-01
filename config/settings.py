15e852cb6562b1507aaf4b7a8454198c7c203d4a
sanjit nc <njsa3803@gmail.com>
2026-06-01 16:56:29 +0530

    Fix allowed hosts

--- config/settings.py
+++ config/settings.py
@@ -155,3 +155,10 @@ MEDIA_ROOT = BASE_DIR / "media"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# STORAGES
+STORAGES = {
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
